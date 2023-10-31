62174d0c20a511f5204a4ac10257d10ea7552192
Jan Galinski <jan.galinski@holisticon.de>
2023-10-31 11:10:02 +0100

    use parent 10.3

--- src/main/kotlin/_camunda-ktx.kt
+++ src/main/kotlin/_camunda-ktx.kt
@@ -1,3 +1,4 @@
 package io.github.jangalinski.camunda
 
 fun TaskId.taskListPath() = "/camunda/app/tasklist/default/#/?task=$this"
+
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,6 +3,7 @@ package io.github.jangalinski.camunda
 import io.swagger.v3.oas.annotations.ExternalDocumentation
 import io.swagger.v3.oas.annotations.OpenAPIDefinition
 import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
@@ -17,10 +18,11 @@ fun main() = runApplication<ExampleApplication>().let { }
     description = "A camunda platform 7 kotlin spring boot showcase.",
     version = "1"
   ),
-  externalDocs = ExternalDocumentation(
-    description = "camunda cockpit",
-    url = "/camunda/app/cockpit/default/#/processes"
-  )
+  tags = [Tag(
+    name = "Camunda",
+    description = "Camunda Platform 7 application",
+    externalDocs = ExternalDocumentation(description = "camunda cockpit", url = "/camunda/app/cockpit/default/#/processes")
+  )]
 )
 class ExampleApplication
 
